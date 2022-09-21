88edf773bc1c00d56bf8148106adf31302dc09e5
Angelos <kounavisangelo@gmail.com>
2022-09-21 19:24:48 -0400

    prompt questions

---
+++ app.js
@@ -0,0 +1,23 @@
+alert("Hello there, Welcome to my Traveling page")
+
+let travelQuestion = prompt ("Do you like to travel?")
+console.log(travelQuestion)
+
+if (travelQuestion == "Yes") or ("yes")
+alert("Great, Let me help you then!");
+    
+
+let userQuestion = prompt ("Would you like to know more about Greece, France or even Italy?")
+console.log(userQuestion)
+
+if(userQuestion == "Yes") or ("yes")
+alert("You are in the right place..")
+
+
+let lastQuestion = prompt ("I am about to show you the most beautiful destinations. Are you ready?")
+console.log(lastQuestion)
+
+if (lastQuestion == "Yes") or ("yes")
+alert("You got it!!");
+
+
