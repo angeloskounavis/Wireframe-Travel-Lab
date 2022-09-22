0d20ad4077a578de39bdd2607ae789d18564d362
Angelos <kounavisangelo@gmail.com>
2022-09-22 15:45:38 -0400

    new function entered

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-alert("Hello there, Welcome to my Traveling page")
+// function functionName(parameters){code to be executed}
+let usersName;
+
+function greetUser() {
+let usersName = prompt("Hi there, what is your name?");
+if (usersName == "") {
+    usersName = prompt("Please enter your name!");
+//document.write("Hi " + usersName + "welcome to my traveling page!!");
+}
+console.log(usersName);
+document.write("Hi " + usersName + " welcome to my traveling page!!");
+return usersName;
+}
+
+greetUser();
+
+//function above is done!
+
 
 let travelQuestion = prompt ("Do you like to travel?")
 console.log(travelQuestion)
@@ -21,3 +38,69 @@ if (lastQuestion == "Yes") or ("yes")
 alert("You got it!!");
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log (greetUser()); 
+//let yourName = greetUser();
+//console.log("Your name here:")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function customAlert(){
+    //if (usersName == "Kassie") {
+     // alert("Hi, there");
+   // } else if (usersName == "Ben") {
+   // alert("Tas are the best");
+   // } else if (usersName == "Aaron"){
+  //      alert("TAs are the best!");
+   // } else if (usersName == "Joey") {
+          //  alert("Tas are the best!");
+   // } else{ 
+         //       alert("Welcome to Class 6, Javascript");
+          //  }
+        
+    
+
+    
+
+
+// create a rule; inside the parenthesis is parameters
+//function addTwoNumbers(ny1, num2){
+//return num1 + num2;
+//}
+// numbers isnide the parenthesis here is arguements
+//addTwoNumbers(5, 10);
